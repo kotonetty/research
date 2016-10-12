7270aada99259a56f5b3729a206a03869bd5d22a
kikuchik <kikuchik@dna.bio.keio.ac.jp>
2016-10-12 16:02:49 +0900

    必要な列のみ抽出

--- get_NPInter_high_confidence.py
+++ get_NPInter_high_confidence.py
@@ -3,30 +3,38 @@
 import csv
 import re
 import codecs
-
+import pandas as pd
+import numpy as np
 
 def change_csv(): #NPInter3のファイルをCSVファイルに変換する関数
-    fp = open("../interaction_NPInter.csv","w")
+    fp = open("interaction_NPInter.csv","w")
 
     #データの読み込み
-    f = codecs.open("../interaction_NPInter[v3.0].txt",encoding="utf-8",errors="ignore")
+#    dataset = pd.read_table("interaction_NPInter[v3.0].txt",encoding='cp037')
+#    dataset.to_csv("test.txt",sep="\t")
+#    dataset.to_csv("test.txt",sep="\t")
+    f = codecs.open("interaction_NPInter[v3.0].txt",encoding="utf-8",errors="ignore")
     lines = f.readlines()
     f.close()
 
     for line in lines:
         row = re.split('\t',line) #行ごとにリスト化
-        row[-1] = row[-1].rstrip('\n')
-        w = csv.writer(fp,delimiter=',')
-        w.writerow(row)
+        if row[12].find('ncRNA-protein') >= 0 or row[0].find('interactionID') >= 0:
+#            row[-1] = row[-1].rstrip('\n') #行末の改行を削除
+            del row[6:9]
+            del row[8:]
+            w = csv.writer(fp,delimiter=',')
+            w.writerow(row)
     fp.close()
+change_csv()
+#データの中からRNA-Proteinの相互作用のデータのみ抜き出す
+#f_csv = open("../interaction_NPInter.csv",'r')
+#f_csv_w = open("../interaction_NPInter2.csv",'w')
+#csvreader = csv.reader(f_csv)
+#header = next(reader)
+#w = csv.writer(f_csv_w,delimiter=',')
+#w.writerow(header) #headerのみ先に書き込む
 
-
-with open('../interaction_NPInter.csv',newline='') as csvfile:
-    csvreader = csv.reader(csvfile,delimiter='')
-    for row in cscreader:
-        
-    if line.find("RNA-Protein") >= 0:
-    print (line[:-1])
-
-
-
+#for row in csvreader: #データの行でncRNA-proteinとなっている行のみ抜き出して書き込む
+#    if row[12].find('ncRNA-protein') >= 0:
+#        w.writerow(row)
